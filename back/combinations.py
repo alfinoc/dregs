2f88ebd2fb8fb3b99c491dc6fb3d20a6a6f00f54
alfinoc <chrisalfino@gmail.com>
2015-04-10 18:30:34 -0400

    initial commit
    Some combination maths.

---
+++ back/combinations.py
@@ -0,0 +1,57 @@
+import math
+
+# Returns a cached version of given 'fn'.
+def cached(fn):
+   cache = {}
+   def checkCache(*args):
+      if args in cache:
+         return cache[args]
+      else:
+         res = fn(*args)
+         cache[args] = res
+         return res
+   return checkCache
+
+# Returns (n choose k).
+def choose(n, k):
+   f = math.factorial
+   if n < k:
+      return 0
+   return f(n) / f(k) / f(n-k)
+
+# Returns maximum value c_k such that (c_k choose k) is less than or equal
+# to 'remaining'.
+def max_ck(k, remaining):
+   n = k
+   while choose(n, k) <= remaining:
+      n += 1
+   return n - 1
+
+# Returns the k-combination corresponding to index i, using the combinatorial
+# number system (see wiki) as a bijection from the natural numbers N to
+# k-combinations of N.
+def combination(k, i):
+   sub = set()
+   while k > 0:
+      c_k = max_ck(k, i)
+      i -= choose(c_k, k)
+      k -= 1
+      sub.add(c_k)
+   return sub
+
+# Returns the bitstring representation of s: the ith bit of the resulting
+# integer is 1 iff i is in s.
+def toBitString(s):
+   res = 0
+   for elt in s:
+      res += 1 << elt
+   return res
+
+# Returns a k-sized subset of {0, ..., n - 1} based on index. Assumes no upper
+# bound on 'index'.
+def subset(n, k, index):
+   return combination(k, index % choose(n, k))
+
+choose = cached(choose)
+max_ck = cached(max_ck)
+combination = cached(combination)
