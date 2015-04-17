e27b717ba496508115bf16de8587ac814001e3bb
alfinoc <chrisalfino@gmail.com>
2015-04-17 18:41:12 -0400

    better issue configuration

--- back/app.py
+++ back/app.py
@@ -1,4 +1,9 @@
 from werkzeug.serving import run_simple
 from wsgi import create_app
+from sys import argv
 
-run_simple('localhost', 5000, create_app(), use_debugger=True, use_reloader=True)
+if len(argv) > 1:
+   app = create_app(argv[1])
+else:
+   app = create_app()
+run_simple('localhost', 5000, app, use_debugger=True, use_reloader=True)
--- back/wsgi.py
+++ back/wsgi.py
@@ -1,11 +1,14 @@
-from os import path 
+from os import path
+from sys import exit
 from service import Service
 from werkzeug.wsgi import SharedDataMiddleware
 
-def create_app():
+def create_app(issue_path='issues/mock'):
    currPath = path.dirname(__file__)
    template_path = path.join(currPath, '../global')
-   issue_path = path.join(currPath, '../issues/mock')
+   issue_path = path.join(currPath, '../' + issue_path)
+   if not path.isdir(issue_path):
+      exit('No issue directory {0}'.format(issue_path))
    app = Service(template_path, issue_path)
    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
       '/global':  path.join(path.dirname(__file__), '../global'),
